1e2a964c670c02f31d57ee0beaa2e61a29c17039
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-04-05 08:34:05 -0700

    testing: Components via npm

---
+++ components/ADSLogo.vue
@@ -0,0 +1,9 @@
+<template>
+	<svg width="50" height="50" viewBox="0 0 50 50" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<rect width="50" height="50" rx="25" fill="#FEBE3F"/>
+		<path d="M37.5 37.5H12.5V30.25H15.3571V34.6429H30.6021L12.5 12.5H16.3471L37.5 37.5Z" fill="#00263A"/>
+		<path d="M37.5 12.5V15.3571H21.385L18.9671 12.5H37.5Z" fill="#00263A"/>
+		<path d="M26.3843 32.6429L12.5 15.6607V20.4879L16.4164 25.3957H12.5V28.2529H18.6964L22.1993 32.6429H26.3843Z" fill="#00263A"/>
+		</svg>
+		
+</template>
