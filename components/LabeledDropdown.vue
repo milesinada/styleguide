83677ab0c646670f8232abf2b21225ced415fe8f
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-02 09:34:09 -0700

    Component: Labeled Dropdown

---
+++ components/LabeledDropdown.vue
@@ -0,0 +1,126 @@
+<template>
+	<div class="dropdown labeled-dropdown">
+		<button
+			@click="toggleDropdown"
+			class="labeled-btn dropdown-caret text-start mb-1"
+		>
+			<label class="within-title px-2">
+				{{ label }}
+			</label>
+			<div class="d-flex justify-content-between">
+				<div class="col-11">
+					{{ selected }}
+				</div>
+				<div class="col-1 text-center align-self-center">
+					<dropdown-caret :pointDown="pointDown" />
+				</div>
+			</div>
+		</button>
+		<div
+			v-if="showDropdown"
+			class="options-container bg-white d-flex flex-column py-2 shadow"
+		>
+			<ul
+				@click="selectOption(option)"
+				v-for="(option, idx) in options"
+				:value="option"
+				:key="idx"
+				class="dropdown-options p-3 mb-0"
+			>
+				{{ option }}
+			</ul>
+		</div>
+	</div>
+</template>
+
+<script>
+import DropdownCaret from "@/components/icons/DropdownCaret.vue";
+export default {
+	components: {
+		DropdownCaret,
+	},
+	emits: ["changeOption"],
+	props: {
+		label: {
+			type: String,
+			default: "Label",
+		},
+		options: {
+			type: Array,
+			default: () => ["option1"],
+		},
+		initialOption: {
+			type: String,
+			default: "Initial Option",
+		},
+	},
+	data() {
+		return {
+			selected: this.initialOption,
+			showDropdown: false,
+			pointDown: true,
+		};
+	},
+	methods: {
+		toggleDropdown() {
+			this.showDropdown = !this.showDropdown;
+			this.pointDown = !this.pointDown;
+		},
+		closeDropdown() {
+			this.showDropdown = false;
+			this.pointDown = true;
+		},
+		selectOption(selectedOption) {
+			this.selected = selectedOption;
+			this.$emit("changeOption", selectedOption);
+			this.closeDropdown();
+		},
+	},
+};
+</script>
+
+<style scoped>
+.labeled-dropdown {
+	position: relative;
+	margin-top: 10px;
+	width: 100%;
+	height: 100%;
+}
+.within-title {
+	position: absolute;
+	top: -7px;
+	left: 10px;
+	font-size: 12px;
+	background-color: white;
+}
+.labeled-btn {
+	background-color: white;
+	width: 100%;
+	height: 100%;
+	padding: 16px;
+	border: 1px solid var(--bs-grayvar-500);
+	border-radius: 4px;
+	font-size: 16px;
+}
+.options-container {
+	position: absolute;
+	border-radius: 5px;
+	width: 100%;
+	font-size: 16px;
+	z-index: 2;
+}
+.dropdown-options {
+	display: flex;
+	background-color: var(--bs-gray-99);
+	height: 46px;
+	flex-direction: row;
+	align-items: center;
+	font-style: normal;
+	font-weight: 400;
+	letter-spacing: 0.5px;
+}
+.dropdown-options:hover {
+	background-color: var(--bs-secondary95);
+	cursor: pointer;
+}
+</style>
