3760bb769edec3ae9be186e4d10e8dfc4f121e1b
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-03 14:49:09 -0700

    Export Component

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
-import * as components from './components.json';
-
-const pkgName = 'miles-styleguide';
-const pkgPath = `/${pkgName}`;
+import LabeledDropdown from './components/LabeledDropdown.vue'
 
 export default {
-  install(app) {
-    Object.entries(components).forEach(([name, path]) => {
-      if (path) {
-        app.component(name, () => import(/* webpackChunkName: "[request]" */ `${pkgPath}/${path}`));
-      }
-    });
-  },
-};
+	install: (app, options) => {
+		app.component("LabeledDropdown", LabeledDropdown)
+	}
+}
