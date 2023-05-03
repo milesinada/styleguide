9715286df512db9a6e14c4cf60d433c590252229
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-03 11:47:11 -0700

    path issues

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import * as components from './components.json';
 export default {
 	install(app) {
 		Object.entries(components).forEach(([name, path]) => {
+			const path = pathObj.value
+			console.log(path)
 			app.component(name, require(`./${path}`).default)
 		})
 	}
