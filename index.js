8fe4f13861b94251bf922b6059f5104132898c1b
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-03 11:49:19 -0700

    pathobj  issues

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import * as components from './components.json';
 
 export default {
 	install(app) {
-		Object.entries(components).forEach(([name, path]) => {
+		Object.entries(components).forEach(([name, pathObj]) => {
 			const path = pathObj.value
 			console.log(path)
 			app.component(name, require(`./${path}`).default)
