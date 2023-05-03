0a7b82cf80fb82b07292f5a446219207670033cf
milesinadaAVA <119697123+milesinadaAVA@users.noreply.github.com>
2023-05-03 11:59:01 -0700

    conditional path

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import * as components from './components.json';
 
 export default {
 	install(app) {
-		Object.entries(components).forEach(([name, pathObj]) => {
-			const path = pathObj.value
-			console.log(path)
-			app.component(name, require(`./${path}`).default)
-		})
-	}
-}
+		Object.entries(components).forEach(([name, path]) => {
+			if (path) {
+			app.component(name, () => import(`./${path}`));
+			}
+		});
+		},
+	};
+	
